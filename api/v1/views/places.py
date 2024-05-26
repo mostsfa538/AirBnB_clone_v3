8abeeaccef28363739ad7aee27506b7b004bcb52
mostsfa538 <mostafa51moktar@gmail.com>
2024-05-26 20:36:34 +0300

    place view

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -69,7 +69,7 @@ def update_place(place_id):
     if data is None:
         abort(400, 'Not a JSON')
     for key, value in data.items():
-        if key not in ['id', 'created_at', 'updated_at']:
+        if key not in ['id', 'user_id', 'city_id', 'created_at', 'updated_at']:
             setattr(place, key, value)
     place.save()
     return jsonify(place.to_dict()), 200
