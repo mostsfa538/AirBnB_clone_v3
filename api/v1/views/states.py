55f91e04e9bf74eee86eafaac079178d3a6af14c
mostsfa538 <mostafa51moktar@gmail.com>
2024-05-26 18:19:01 +0300

    view of state

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -37,7 +37,7 @@ def delete_state(state_id):
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
     """ create state """
-    data = request.json
+    data = request.get_json()
     if data is None:
         abort(400, 'Not a JSON')
     if 'name' not in data:
@@ -47,7 +47,7 @@ def create_state():
     return jsonify(new_state.to_dict()), 201
 
 
-@app_views.route('states/<state_id>', methods=['PUT'],
+@app_views.route('/states/<state_id>', methods=['PUT'],
                  strict_slashes=False)
 def update_state(state_id):
     """ update state """
