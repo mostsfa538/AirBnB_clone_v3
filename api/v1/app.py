c0ed0d492df1ca6dc32e99bcb679e240065cd712
mostsfa538 <mostafa51moktar@gmail.com>
2024-05-26 13:38:06 +0300

    status of api

---
+++ api/v1/app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Here goes every thing """
+import os
+from flask import Flask, jsonify
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def treardown(err):
+    """  close """
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """not_found"""
+    return jsonify({"error": "Not found"}), 404
+
+
+if __name__ == "__main__":
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', '5000'))
+    app.run(host=host, port=port, threaded=True)
