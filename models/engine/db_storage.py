275e625d617fb039829c7f775ef26356572d354c
mostsfa538 <mostafa51moktar@gmail.com>
2024-05-26 11:26:30 +0300

    update storge

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -75,29 +75,14 @@ class DBStorage:
         """call remove() method on the private session attribute"""
         self.__session.remove()
 
-    def count(self, cls=None):
-        """ Returns the number of objects in storage matching the given class.
-            If no class is passed, returns the count of all objects in storage
-        """
-        count = 0
-        if cls is None:
-            for clss in classes:
-                objs = self.__session.query(classes[clss]).all()
-                for obj in objs:
-                    count += 1
-        else:
-            if cls in classes:
-                objs = self.__session.query(classes[cls]).all()
-                for obj in objs:
-                    count += 1
-        return count
-
     def get(self, cls, id):
-        """
-            Returns the object based on the class and its ID,
-            or None if not found
-        """
-        if id:
-            obj = self.__session.query(classes[cls]).filter(id == self.id)
-            return obj
+        """ A method to retrieve one object """
+        get_cls = self.all(cls)
+        for key, val in get_cls.items():
+            if id == str(val.id):
+                return val
         return None
+
+    def count(self, cls=None):
+        """ A method to count the number of objects in storage """
+        return len(self.all(cls))
